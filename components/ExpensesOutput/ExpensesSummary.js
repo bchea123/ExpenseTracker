9f38d04fe55a68792fc7951361fd1f3397efdd72
Brandon's Laptop <brandon.chea@yahoo.com>
2023-11-10 01:34:04 -0800

    Added expenses components

---
+++ components/ExpensesOutput/ExpensesSummary.js
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+
+function ExpensesSummary() {
+    return (
+        <View>
+            <Text>Lasy 7 Days</Text>
+            <Text>$177.95</Text>
+        </View>
+    );
+}
+
+export default ExpensesSummary;
