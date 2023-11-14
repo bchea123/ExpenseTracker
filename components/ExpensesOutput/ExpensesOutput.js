1bc8b13a3781fa686a29d5ebeea228b2970c2766
bchea123 <brandon.chea@yahoo.com>
2023-11-13 23:38:25 -0800

    Began added expenses logic and created array of dummy expenses

--- components/ExpensesOutput/ExpensesOutput.js
+++ components/ExpensesOutput/ExpensesOutput.js
@@ -2,11 +2,44 @@ import { View } from "react-native";
 import ExpensesSummary from "./ExpensesSummary";
 import ExpensesList from "./ExpensesList";
 
-function ExpensesOutput({expenses}) {
+const DUMMY_EXPENSES = [
+    {
+        id: 'e1',
+        description: 'A pair of shoes',
+        amount: 59.99,
+        date: new Date('2023-11-10')
+    },
+    {
+        id : 'e2',
+        description: 'A pair of trousers',
+        amount: 89.29,
+        date: new Date('2022-01-05')
+    },
+    {
+        id : 'e3',
+        description: 'Some bananas',
+        amount: 5.99,
+        date: new Date('2023-10-05')
+    },
+    {
+        id : 'e4',
+        description: 'A book',
+        amount: 14.99,
+        date: new Date('2022-02-19')
+    },
+    {
+        id : 'e5',
+        description: 'A book',
+        amount: 18.59,
+        date: new Date('2022-02-18')
+    }
+]
+
+function ExpensesOutput({expenses, expensesPeriod}) {
     return (
         <View>
-            <ExpensesSummary />
-            <ExpensesList />
+            <ExpensesSummary expenses={DUMMY_EXPENSES} periodName={expensesPeriod} />
+            <ExpensesList  expenses={DUMMY_EXPENSES} />
         </View>
     );
 }
