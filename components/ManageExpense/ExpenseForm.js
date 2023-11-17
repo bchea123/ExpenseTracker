c8308068e57f09f4f07b39585e41d675bf5835f6
bchea123 <brandon.chea@yahoo.com>
2023-11-16 23:38:13 -0800

    Added validation for user inputs on manage expense screen

--- components/ManageExpense/ExpenseForm.js
+++ components/ManageExpense/ExpenseForm.js
@@ -1,13 +1,14 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Alert } from "react-native";
 import Input from "./Input";
 import Button from '../ui/Button'
 import { useState } from "react";
+import { getFormattedDate } from "../../util/date";
 
-function ExpenseForm({submitButtonLabel, onCancel, onSubmit}){
+function ExpenseForm({submitButtonLabel, onCancel, onSubmit, defaultValues}){
     const [inputValues, setInputValues] = useState({
-        amount: '',
-        date: '',
-        description: ''
+        amount: defaultValues ? defaultValues.amount.toString() : '',
+        date: defaultValues ? getFormattedDate(defaultValues.date) : '',
+        description: defaultValues ? defaultValues.description : ''
     });
 
     function inputChangedHandler(inputIdentifier, enteredValue){
@@ -25,6 +26,15 @@ function ExpenseForm({submitButtonLabel, onCancel, onSubmit}){
             date: new Date(inputValues.date),
             description: inputValues.description
         };
+
+        const amountIsValid = !isNaN(expenseData.amount) && expenseData.amount > 0;
+        const dateIsValid = expenseData.date.toString() !== 'Invalid Date';
+        const descriptionIsValid = expenseData.description.trim().length > 0;
+
+        if (!amountIsValid || !dateIsValid || !descriptionIsValid) {
+            Alert.alert('Invalid input', 'Please check your input values')
+            return;
+        }
 
         onSubmit(expenseData);
     }
