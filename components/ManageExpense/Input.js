0656e06065c5c98690ab60aebeebf19a7d945b04
bchea123 <brandon.chea@yahoo.com>
2023-11-16 22:50:39 -0800

    Styled manage expense screen

--- components/ManageExpense/Input.js
+++ components/ManageExpense/Input.js
@@ -1,10 +1,17 @@
 import { StyleSheet, Text, TextInput, View } from "react-native";
+import { GlobalStyles } from "../../constants/styles";
 
-function Input({label, textInputConfig}){
+function Input({label, style, textInputConfig}){
+    const inputStyles = [styles.input];
+
+    if (textInputConfig && textInputConfig.multiline) {
+        inputStyles.push(styles.inputMultiline);
+    }
+
     return (
-        <View>
-            <Text>{label}</Text>
-            <TextInput {...textInputConfig} />
+        <View style={[styles.inputContainer, style]}>
+            <Text style={styles.label}>{label}</Text>
+            <TextInput {...textInputConfig} style={inputStyles}/>
         </View>
     );
 }
@@ -12,5 +19,24 @@ function Input({label, textInputConfig}){
 export default Input;
 
 const styles = StyleSheet.create({
-
+    inputContainer: {
+        marginHorizontal: 4,
+        marginVertical: 8,
+    },
+    label: {
+        fontSize: 12,
+        color: GlobalStyles.colors.primary100,
+        marginBottom: 4
+    },
+    input: {
+        backgroundColor: GlobalStyles.colors.primary100,
+        color: GlobalStyles.colors.primary700, 
+        padding: 6,
+        borderRadius: 6,
+        fontSize: 18
+    },
+    inputMultiline: {
+        minHeight: 100,
+        textAlignVertical: 'top'
+    }
 });
